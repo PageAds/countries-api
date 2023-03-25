b9d6ad641566b111bab2f898fcc9f37f8aad5fcc
PageAds <adampage6@gmail.com>
2023-03-25 22:55:00 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CodingChallenge
+Paymentsense coding challenge
+
+## Setup
+* Please download latest dotnet core (7+) -> https://dotnet.microsoft.com/download/dotnet-core
---
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api.Tests/HealthcheckTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Paymentsense.Coding.Challenge.Api.Tests
+{
+    public class HealthcheckTests
+    {
+        private readonly HttpClient _client;
+
+        public HealthcheckTests()
+        {
+            var builder = new WebHostBuilder().UseStartup<Startup>();
+            var testServer = new TestServer(builder);
+            _client = testServer.CreateClient();
+        }
+
+        [Fact]
+        public async Task Health_OnInvoke_ReturnsHealthy()
+        {
+            var response = await _client.GetAsync("/health");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            responseString.Should().Be("Healthy");
+        }
+    }
+}
