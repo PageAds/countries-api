e57ab103c28d310a1b7e7cce68159ab2403c6c35
PageAds <adampage6@gmail.com>
2023-03-27 08:54:17 +0100

    TDD - Integration test passed (returning paginated response)

--- paymentsense-coding-challenge-api/src/Countries.Api/Controllers/CountriesController.cs
+++ paymentsense-coding-challenge-api/src/Countries.Api/Controllers/CountriesController.cs
@@ -1,6 +1,9 @@
-﻿using Countries.Application.Models;
+﻿using Countries.Api.Filters;
+using Countries.Application.Models;
 using Countries.Domain.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Countries.Controllers
@@ -17,13 +20,23 @@ namespace Countries.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<CountriesResponse>> Get()
+        public async Task<ActionResult<CountriesResponse>> Get([FromQuery] PaginationFilter filter)
         {
+            var paginationFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
+
             var countries = await this.countriesRepository.Get();
+
+            var countriesPaginated = countries
+                .Skip((paginationFilter.PageNumber - 1) * paginationFilter.PageSize)
+                .Take(paginationFilter.PageSize);
 
             var countriesResponse = new CountriesResponse()
             {
-                Countries = countries
+                PageNumber = paginationFilter.PageNumber,
+                PageSize = paginationFilter.PageSize,
+                TotalRecords = countries.Count(),
+                TotalPages = (int)Math.Ceiling((double)countries.Count() / (double)paginationFilter.PageSize),
+                Countries = countriesPaginated
             };
 
             return Ok(countriesResponse);
---
+++ paymentsense-coding-challenge-api/src/Countries.Api/Filters/PaginationFilter.cs
@@ -0,0 +1,23 @@
+﻿namespace Countries.Api.Filters
+{
+    public class PaginationFilter
+    {
+        private const int MaxPageSize = 20;
+
+        public PaginationFilter()
+        {
+            this.PageNumber = 1;
+            this.PageSize = MaxPageSize;
+        }
+
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            this.PageSize = pageSize > MaxPageSize ? MaxPageSize : pageSize;
+        }
+
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
